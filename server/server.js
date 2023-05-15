f893d5203494484a967a5f04692b5836b09ebb25
AndreBlankholm <ablankholm@gmail.com>
2023-05-15 13:33:42 -0500

    added npm packages

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./config/connection');
+const db = require('./config/connection');  //the exported mongoose connection
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+db.once('open', () => {  // looking for the connection to be made using db-open
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    console.log('succesful server connection using mongoose');
   });
 });
